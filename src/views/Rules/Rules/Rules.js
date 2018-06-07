30ba50f18e26a2644d42e9154d3d14a88a30d860
Sergio Muñoz <sergiomunlop@gmail.com>
2018-06-07 10:49:25 +0200

    base dnd

--- src/views/Rules/Rules/Rules.js
+++ src/views/Rules/Rules/Rules.js
@@ -1,15 +1,122 @@
 import React, { Component } from 'react';
-import { Col, Row, Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
+import { Card, CardHeader, CardBody, ListGroup, ListGroupItem, Col, Row, Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
 import classnames from 'classnames';
 import RuleItem from './RuleItem';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+// fake data generator
+const getItems = (count, offset = 0) =>
+    Array.from({ length: count }, (v, k) => k).map(k => ({
+        id: `item-${k + offset}`,
+        content: `item ${k + offset}`
+    }));
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
+
+/**
+ * Moves an item from one list to another list.
+ */
+const move = (source, destination, droppableSource, droppableDestination) => {
+    const sourceClone = Array.from(source);
+    const destClone = Array.from(destination);
+    const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+    destClone.splice(droppableDestination.index, 0, removed);
+
+    const result = {};
+    result[droppableSource.droppableId] = sourceClone;
+    result[droppableDestination.droppableId] = destClone;
+
+    return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: 'none',
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+
+    // change background colour if dragging
+    background: isDragging ? 'white' : 'white',
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? 'lightblue' : 'lightgrey',
+    padding: grid,
+    width: 250
+});
 
 class Rules extends Component {
+
+
+    /**
+     * A semi-generic way to handle multiple lists. Matches
+     * the IDs of the droppable container to the names of the
+     * source arrays stored in the state.
+     */
+    id2List = {
+        droppable: 'items',
+        droppable2: 'selected'
+    };
+
+    getList = id => this.state[this.id2List[id]];
+
+    onDragEnd = result => {
+        const { source, destination } = result;
+
+        // dropped outside the list
+        if (!destination) {
+            return;
+        }
+
+        if (source.droppableId === destination.droppableId) {
+            const items = reorder(
+                this.getList(source.droppableId),
+                source.index,
+                destination.index
+            );
+
+            let state = { items };
+
+            if (source.droppableId === 'droppable2') {
+                state = { selected: items };
+            }
+
+            this.setState(state);
+        } else {
+            const result = move(
+                this.getList(source.droppableId),
+                this.getList(destination.droppableId),
+                source,
+                destination
+            );
+
+            this.setState({
+                items: result.droppable,
+                selected: result.droppable2
+            });
+        }
+    };
 
     constructor(props){
         super(props);
         this.toggle = this.toggle.bind(this);
         this.state = {
             activeTab: '1',
+            items: getItems(10),
+            selected: getItems(5, 10)
         };
     }
 
@@ -21,6 +128,7 @@ class Rules extends Component {
         }
       }
 
+      
     render (){
         return (
             <div className="animated fadeIn">
@@ -51,7 +159,104 @@ class Rules extends Component {
                         </TabPane>
                         <TabPane tabId="2">
                         <Row>
-                            </Row>
+                        <DragDropContext onDragEnd={this.onDragEnd}>
+
+                        <Col sm="3" xl="3">
+                            <Card>
+                            <CardHeader>
+                                <i className="fa fa-align-justify"></i><strong>List Group</strong>
+                                <div className="card-header-actions">
+                                <a href="https://reactstrap.github.io/components/listgroup/" rel="noreferrer noopener" target="_blank" className="card-header-action">
+                                    <small className="text-muted">docs</small>
+                                </a>
+                                </div>
+                            </CardHeader>
+                            <CardBody>
+                            <Droppable droppableId="droppable">
+                                {(provided, snapshot) => (
+                                        <ListGroup>
+                                        <div
+                                            ref={provided.innerRef}
+                                            style={getListStyle(snapshot.isDraggingOver)}>
+                                            {this.state.items.map((item, index) => (
+                                                <Draggable
+                                                    key={item.id}
+                                                    draggableId={item.id}
+                                                    index={index}>
+                                                    {(provided, snapshot) => (
+                                                        <ListGroupItem>
+                                                        <div
+                                                            ref={provided.innerRef}
+                                                            {...provided.draggableProps}
+                                                            {...provided.dragHandleProps}
+                                                            style={getItemStyle(
+                                                                snapshot.isDragging,
+                                                                provided.draggableProps.style
+                                                            )}>
+                                                            {item.content}
+                                                        </div>
+                                                        </ListGroupItem>
+                                                    )}
+                                                </Draggable>
+                                            ))}
+                                            {provided.placeholder}
+                                        </div>
+                                        </ListGroup>
+                                    )}
+                                </Droppable>
+                            </CardBody>
+                            </Card>
+                        </Col>
+                        <Col sm="3" xl="3">
+                            <Card>
+                            <CardHeader>
+                                <i className="fa fa-align-justify"></i><strong>List Group</strong>
+                                <div className="card-header-actions">
+                                <a href="https://reactstrap.github.io/components/listgroup/" rel="noreferrer noopener" target="_blank" className="card-header-action">
+                                    <small className="text-muted">docs</small>
+                                </a>
+                                </div>
+                            </CardHeader>
+                            <CardBody>
+                            <Droppable droppableId="droppable2">
+                                {(provided, snapshot) => (
+                                    <ListGroup>
+                                    <div
+                                        ref={provided.innerRef}
+                                        style={getListStyle(snapshot.isDraggingOver)}>
+                                        {this.state.selected.map((item, index) => (
+                                            <ListGroupItem>
+                                            <Draggable
+                                                key={item.id}
+                                                draggableId={item.id}
+                                                index={index}>
+                                                {(provided, snapshot) => (
+                                                    <div
+                                                        ref={provided.innerRef}
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}
+                                                        style={getItemStyle(
+                                                            snapshot.isDragging,
+                                                            provided.draggableProps.style
+                                                        )}>
+                                                        {item.content}
+                                                    </div>
+                                                )}
+                                            </Draggable>
+                                            </ListGroupItem>
+                                        ))}
+                                        {provided.placeholder}
+                                    </div>
+                                    </ListGroup>
+                                )}
+                            </Droppable>
+                            </CardBody>
+                            </Card>
+                        </Col>
+                        </DragDropContext>
+
+                       
+                        </Row>
                         </TabPane>
                     </TabContent>
                 </Col>
