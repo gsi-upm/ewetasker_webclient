dfb7921b275db33cc7d693b2668994ecb283cd4b
Unknown <enriquetolby@gmail.com>
2018-10-24 13:09:11 +0200

    ewetasker logo fixed

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import {AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
-import logo from '../../assets/img/brand/logo.svg'
-import sygnet from '../../assets/img/brand/sygnet.svg'
+import logo from '../../assets/img/brand/ewetasker.png'
+import sygnet from '../../assets/img/brand/ewetasker_logo.png'
 
 const propTypes = {
   children: PropTypes.node,
@@ -21,8 +21,8 @@ class DefaultHeader extends Component {
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />
         <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
-          minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
+          full={{ src: logo, height: 80, alt: 'Ewetasker Img' }}
+          minimized={{ src: sygnet, height: 40, alt: 'Ewetasker Logo' }}
         />
 
 
